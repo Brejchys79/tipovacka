9bbf2a553fe1d7fa95aa790f33c6aa0dc376a907
Brejchys79 <brejchajakub79@gmail.com>
2025-09-05 12:48:31 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKs0rNk-TCwXipMoeASe90bq3Cl9zKFnU",
-    authDomain: "tipovacka-c1a1b.firebaseapp.com",
-    databaseURL: "https://tipovacka-c1a1b-default-rtdb.firebaseio.com",
-    projectId: "tipovacka-c1a1b",
-    storageBucket: "tipovacka-c1a1b.appspot.com",
-    messagingSenderId: "841726766663",
-    appId: "1:841726766663:web:295099985d8af6e31d8b62"
+ apiKey: "AIzaSyAKs0rNk-TCwXipMoeASe90bq3Cl9zKFnU",
+  authDomain: "tipovacka-c1a1b.firebaseapp.com",
+  databaseURL: "https://tipovacka-c1a1b-default-rtdb.firebaseio.com",
+  projectId: "tipovacka-c1a1b",
+  storageBucket: "tipovacka-c1a1b.firebasestorage.app",
+  messagingSenderId: "841726766663",
+  appId: "1:841726766663:web:56905770a465caf01d8b62",
+  measurementId: "G-EJY747TR53"
   };
 
 const app = initializeApp(firebaseConfig)
