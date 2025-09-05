f5f3c40e6ee2198ec7dc71ec9fcfb2b3727ad25d
Brejchys79 <brejchajakub79@gmail.com>
2025-09-05 13:02:33 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
-// Firebase init
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
+// Firebase konfigurace
 const firebaseConfig = {
- apiKey: "AIzaSyAKs0rNk-TCwXipMoeASe90bq3Cl9zKFnU",
+  apiKey: "AIzaSyAKs0rNk-TCwXipMoeASe90bq3Cl9zKFnU",
   authDomain: "tipovacka-c1a1b.firebaseapp.com",
   databaseURL: "https://tipovacka-c1a1b-default-rtdb.firebaseio.com",
   projectId: "tipovacka-c1a1b",
@@ -12,8 +14,13 @@ const firebaseConfig = {
   messagingSenderId: "841726766663",
   appId: "1:841726766663:web:56905770a465caf01d8b62",
   measurementId: "G-EJY747TR53"
-  };
+};
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getDatabase(app)
+// Inicializace Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+// Export pro použití v React aplikaci
+export { app, database, auth, analytics };
